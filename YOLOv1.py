ece5ebe1bab9708c29e3f8254be4dddf3fe908ed
Elisa Troschka <adcy353@gpu04.pri.hyperion.alces.network>
2023-03-20 17:47:34 +0000

    Added fc option

--- YOLOv1.py
+++ YOLOv1.py
@@ -43,7 +43,7 @@ class YOLOv1(nn.Module):
     *YOLO v1*
     Implementation of YOLO v1 architecture
     """
-    def __init__(self, input_channels, arch=None, S=7, B=2, C=4):
+    def __init__(self, input_channels, arch=None, S=7, B=2, C=4, fc=2):
         """
         :param input_channels: number of input channels
         :param arch: description of the network architecture
@@ -103,7 +103,7 @@ class YOLO(nn.Module):
     *YOLO v1*
     Implementation of YOLO v1 architecture
     """
-    def __init__(self, input_channels, arch="darknetv1", S=7, B=2, C=4):
+    def __init__(self, input_channels, arch="darknetv1", S=7, B=2, C=4, fc=2):
         """
         :param input_channels: number of input channels
         :param arch: description of the network architecture
@@ -119,13 +119,8 @@ class YOLO(nn.Module):
         self.in_channels = input_channels
         self.darknet = self._create_darknet()
         self.avgpool = nn.AdaptiveAvgPool2d((S, S))
-        self.fc = nn.Linear(1024, S * S * (B * 5 + C))
+        self._create_fc(fc)
         
-        self.fc = nn.Sequential(
-            nn.Linear(1024 * S * S, 4096),
-            nn.LeakyReLU(0.1),
-            nn.Linear(4096, S * S * (B * 5 + C))
-        )
 
     def forward(self, x):
         out = self.darknet(x)
@@ -133,71 +128,22 @@ class YOLO(nn.Module):
         out = torch.flatten(out, start_dim=1)
         out = self.fc(out)
         return out
-
-    def _create_darknet(self):
-        """
-        Creates the darknet layers
-        """
-        n_in = self.in_channels
-        darknet = []
-        for layer in self.architecture:
-            block, n_in = self._make_block(n_in, *layer)
-            darknet.append(block)
-        return nn.Sequential(*darknet)
-
-    def _make_block(self, n_int, convs, type=None):
-        """
-        Creates a block of ConvLayer, given their parameters (convs).
-        If maxpool = True, it adds a final MaxPool2d layer.
-        """
-        layers = []
-        n_input = n_int
-        for c in convs:
-            layers.append(Convlayer(n_input, *c))
-            n_input = c[0]
-        n_out = layers[-1].out_channels
-        if type == 'maxpool':
-            layers.append(nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)))
-            return nn.Sequential(*layers), n_out
-        elif type == 'residual':
-            return ResidualBlock(layers), n_out
-        return nn.Sequential(*layers), n_out
-
-class YOLO2(nn.Module):
-    """
-    *YOLO v1*
-    Implementation of YOLO v1 architecture
-    """
-    def __init__(self, input_channels, arch="darknetv1", S=7, B=2, C=4):
-        """
-        :param input_channels: number of input channels
-        :param arch: description of the network architecture
-        :param S: size of the squares
-        :param B: number of boxes per square
-        :param C: number of classes
-        """
-        super(YOLO2, self).__init__()
-        self.architecture = pickle.load(open(f"Architectures/{arch}.pkl", "rb"))
-        self.in_channels = input_channels
-        self.darknet = self._create_darknet()
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc1 = nn.Linear(1024, 4096)  
-        self.LeakyReLU = nn.LeakyReLU(0.1)
-        self.fc2 = nn.Linear(4096, S*S*(B*5+C))
-        self.S = S
-        self.B = B 
-        self.C = C
-
-    def forward(self, x):
-        out = self.darknet(x)
-        out = self.avgpool(out)
-        out = torch.flatten(out, start_dim=1)
-        out = self.fc1(out)
-        out = self.LeakyReLU(out)
-        out = self.fc2(out)
+    
+    
+    def _create_fc(self, fc):
+        S, B, C = self.S, self.B, self.C
+        if fc == 1:
+            self.fc == nn.Linear(1024 * S * S, S * S * (B * 5 + C))
+        else:
+            l = [nn.Linear(1024 * S * S, 4096), nn.LeakyReLU(0.1)]
+            for _ in range(fc - 2):
+                l.append(nn.Linear(4096, 4096))
+                l.append(nn.LeakyReLU(0.1))
+            l.append(nn.Linear(4096, S * S * (B * 5 + C)))
         
-        return out
+        self.fc = nn.Sequential(*l)
 
+    
     def _create_darknet(self):
         """
         Creates the darknet layers
@@ -209,6 +155,7 @@ class YOLO2(nn.Module):
             darknet.append(block)
         return nn.Sequential(*darknet)
 
+    
     def _make_block(self, n_int, convs, type=None):
         """
         Creates a block of ConvLayer, given their parameters (convs).
@@ -226,3 +173,4 @@ class YOLO2(nn.Module):
         elif type == 'residual':
             return ResidualBlock(layers), n_out
         return nn.Sequential(*layers), n_out
+

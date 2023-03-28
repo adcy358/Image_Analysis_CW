8a53867b6a46c0ac2166215461ca94a075928081
ElisaTroschka <80113728+ElisaTroschka@users.noreply.github.com>
2023-03-28 15:53:57 +0100

    Add files via upload
    test val split

---
+++ train_val_split.py.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+
+classes = {c: len(os.listdir(f'archive/{c}/'))//2 for c in os.listdir('archive/')}
+classes
+
+old_path = 'archive/'
+new_path = "African_Wildlife_with_val/"
+train_size = int(0.6 * 376)
+test_val_size = (376 - train_size) // 2
+
+for idx, c in enumerate(classes.keys()):
+    fnames = os.listdir(f'archive/{c}/')
+    fnum = set(f.split('.')[0] for f in fnames)
+    test_split = np.random.choice([x for x in fnum], size=test_val_size, replace=False)
+    val_split = np.random.choice([x for x in fnum if x not in test_split], size=test_val_size, replace=False)
+    for i,f in enumerate(fnum):
+        name = str(i + 376 * idx)
+        if f in test_split:
+            os.rename(f'{old_path}{c}/{f}.jpg', f'{new_path}test/{name}.jpg')
+        elif f in val_split:
+            os.rename(f'{old_path}{c}/{f}.jpg', f'{new_path}val/{name}.jpg')
+        else:
+            os.rename(f'{old_path}{c}/{f}.jpg', f'{new_path}train/{name}.jpg')
+        os.rename(f'{old_path}{c}/{f}.txt', f'{new_path}annotations/{name}.txt')
+
